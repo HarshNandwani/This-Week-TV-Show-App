baa2ee4b07760589a5b1a6407608cbd89041fa1a
Harsh Nandwani <nandwaniharsh@gmail.com>
2024-01-13 17:01:57 +0530

    Added @HiltAndroidApp annotation to application class and made functions suspend in RetrofitApi abstraction

--- app/src/main/java/com/harsh/samples/thisweektvshow/ThisWeekTvShowApp.kt
+++ app/src/main/java/com/harsh/samples/thisweektvshow/ThisWeekTvShowApp.kt
@@ -1,5 +1,7 @@
 package com.harsh.samples.thisweektvshow
 
 import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class ThisWeekTvShowApp : Application()
--- app/src/main/java/com/harsh/samples/thisweektvshow/data/remote/TheMovieDbApi.kt
+++ app/src/main/java/com/harsh/samples/thisweektvshow/data/remote/TheMovieDbApi.kt
@@ -9,14 +9,14 @@ import retrofit2.http.Query
 
 interface TheMovieDbApi {
     @GET("/3/trending/tv/week")
-    fun getTrendingShowsThisWeek(): Response<TvShowsResponseDto>
+    suspend fun getTrendingShowsThisWeek(): Response<TvShowsResponseDto>
 
     @GET("/3/tv/{id}")
-    fun getTvShowDetails(@Path("id") showId: Long): Response<DetailedTvShowDto>
+    suspend fun getTvShowDetails(@Path("id") showId: Long): Response<DetailedTvShowDto>
 
     @GET("/3/search/tv")
-    fun getSearchedTvShows(@Query("query") query: String): Response<TvShowsResponseDto>
+    suspend fun getSearchedTvShows(@Query("query") query: String): Response<TvShowsResponseDto>
 
     @GET("/3/tv/{id}/similar")
-    fun getSimilarTvShows(@Path("id") showId: Long): Response<TvShowsResponseDto>
+    suspend fun getSimilarTvShows(@Path("id") showId: Long): Response<TvShowsResponseDto>
 }

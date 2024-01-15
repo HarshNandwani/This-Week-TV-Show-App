dec1f06e242a1f0c7474860a7cb21b393ff5f5fa
Harsh Nandwani <nandwaniharsh@gmail.com>
2024-01-15 18:07:38 +0530

    Bump version after 1.0 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,8 +15,8 @@ android {
         applicationId = "com.harsh.samples.thisweektvshow"
         minSdk = 26
         targetSdk = 34
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.0.1"
 
         kapt {
             arguments {
